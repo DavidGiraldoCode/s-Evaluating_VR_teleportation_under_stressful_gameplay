d97233212f24dbe57f6ecee5f7f7d7df82e19d26
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-10-16 16:33:35 +0200

    Color prompt GUI shows and hides with states

--- Assets/Scripts/Managers/GameplayGUIManager.cs
+++ Assets/Scripts/Managers/GameplayGUIManager.cs
@@ -5,10 +5,9 @@ using UnityEngine;
 public class GameplayGUIManager : MonoBehaviour
 {
     public static GameplayGUIManager Instance { get; private set; }
-    [SerializeField] private GameObject m_startPracticeGUI;
     [SerializeField] private GameObject m_startGameGUI;
-    [SerializeField] private GameObject m_startTrialGUI;
     [SerializeField] private GameObject m_counterGUI;
+    private ColorPromptController m_colorPromptController;
     #region Monobehavior
     private void Awake()
     {
@@ -20,6 +19,9 @@ public class GameplayGUIManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        m_colorPromptController = FindFirstObjectByType<ColorPromptController>();
+        m_colorPromptController.gameObject.SetActive(false);
     }
     private void Start()
     {
@@ -37,6 +39,13 @@ public class GameplayGUIManager : MonoBehaviour
         GameplayManager.OnTrialStandby += OnTrialStandby;
         GameplayManager.OnTrialBegin += OnTrialBegin;
         GameplayManager.OnTrialEnd += OnTrialEnd;
+
+
+        GameplayManager.OnPracticeBegin += OnTaskBegin;
+        GameplayManager.OnTrialBegin += OnTaskBegin;
+
+        GameplayManager.OnPracticeEnd += OnTaskEnd;
+        GameplayManager.OnTrialEnd += OnTaskEnd;
     }
 
     private void OnDisable()
@@ -48,49 +57,57 @@ public class GameplayGUIManager : MonoBehaviour
         GameplayManager.OnTrialStandby -= OnTrialStandby;
         GameplayManager.OnTrialBegin -= OnTrialBegin;
         GameplayManager.OnTrialEnd -= OnTrialEnd;
-    }
 
+        GameplayManager.OnPracticeBegin -= OnTaskBegin;
+        GameplayManager.OnTrialBegin -= OnTaskBegin;
+
+        GameplayManager.OnPracticeEnd += OnTaskEnd;
+        GameplayManager.OnTrialEnd += OnTaskEnd;
+    }
 
     #endregion Monobehavior
     #region Events handlers
+    private void OnTaskBegin()
+    {
+        m_startGameGUI.SetActive(false);
+        m_colorPromptController.gameObject.SetActive(true);
+    }
+    private void OnTaskEnd()
+    {
+        m_colorPromptController.gameObject.SetActive(false);
+        m_counterGUI.SetActive(true);
+    }
+
     private void OnPracticeStandby()
     {
-        m_startPracticeGUI.SetActive(true);
-        
-        m_startTrialGUI.SetActive(false);
-
+        m_counterGUI.SetActive(false);
         m_startGameGUI.SetActive(true);
         m_startGameGUI.GetComponentInChildren<TMP_Text>().text = "Start the practice whenever you are ready";
-        m_counterGUI.SetActive(false);
     }
+
     private void OnPracticeBegin()
     {
-        m_startPracticeGUI.SetActive(false);
-
-        m_startGameGUI.SetActive(false);
+        //m_startGameGUI.SetActive(false);
     }
     private void OnPracticeEnd()
     {
-        m_counterGUI.SetActive(true);
+        //m_counterGUI.SetActive(true);
     }
     private void OnTrialStandby()
     {
         m_counterGUI.SetActive(false);
-        m_startTrialGUI.SetActive(true);
-
         m_startGameGUI.SetActive(true);
         m_startGameGUI.GetComponentInChildren<TMP_Text>().text = "Start the trial whenever you are ready";
     }
     private void OnTrialBegin()
     {
-        m_startTrialGUI.SetActive(false);
-
-        m_startGameGUI.SetActive(false);
+        //m_startGameGUI.SetActive(false);
     }
     private void OnTrialEnd()
     {
-        m_counterGUI.SetActive(true);
+        //m_counterGUI.SetActive(true);
     }
+
 
     #endregion Events
 }

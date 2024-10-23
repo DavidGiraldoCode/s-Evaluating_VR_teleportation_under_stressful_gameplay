046ad4163fbc6134692b0f7d6ecf76bf55a25870
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-10-23 17:27:57 +0200

    Enabled A to activate rotation, and left joystick to define rotation

--- Assets/Scripts/Controllers/VR_Dependent/ControllerDataReader.cs
+++ Assets/Scripts/Controllers/VR_Dependent/ControllerDataReader.cs
@@ -6,37 +6,38 @@ using Oculus.Interaction.Input;
 /// </summary>
 public class ControllerDataReader : MonoBehaviour
 {
-    ControllerRef _controllerRef;
+    public ControllerRef _rightControllerRef;
+    public ControllerRef _leftControllerRef;
+
     public Transform _gameObjTransform;
 
     private void Awake()
     {
-        _controllerRef = GetComponent<ControllerRef>();
     }
 
     private void Update()
     {
-        if (_controllerRef.ControllerInput.TriggerButton)
+        if (_rightControllerRef.ControllerInput.PrimaryButton)
         {
-            Vector3 joyStickRotation = new Vector3(_controllerRef.ControllerInput.Primary2DAxis.x, 0.0f, _controllerRef.ControllerInput.Primary2DAxis.y);
+            Vector3 joyStickRotation = new Vector3(_leftControllerRef.ControllerInput.Primary2DAxis.x, 0.0f, _leftControllerRef.ControllerInput.Primary2DAxis.y);
             _gameObjTransform.rotation = Quaternion.LookRotation(joyStickRotation, Vector3.up);
         }
     }
 
     private void PrinterOfInput()
     {
-        Debug.Log("GripButton:..........:" + _controllerRef.ControllerInput.GripButton);
-        Debug.Log("TriggerButton:.......:" + _controllerRef.ControllerInput.TriggerButton);
-        Debug.Log("MenuButton:..........:" + _controllerRef.ControllerInput.MenuButton);
-        Debug.Log("Thumbrest:...........:" + _controllerRef.ControllerInput.Thumbrest);
-        Debug.Log("Primary2DAxis:.......:" + _controllerRef.ControllerInput.Primary2DAxis);
-        Debug.Log("Primary2DAxisClick:..:" + _controllerRef.ControllerInput.Primary2DAxisClick);
-        Debug.Log("Primary2DAxisTouch:..:" + _controllerRef.ControllerInput.Primary2DAxisTouch);
-        Debug.Log("PrimaryButton:.......:" + _controllerRef.ControllerInput.PrimaryButton);
-        Debug.Log("PrimaryTouch:........:" + _controllerRef.ControllerInput.PrimaryTouch);
-        Debug.Log("SecondaryButton:.....:" + _controllerRef.ControllerInput.SecondaryButton);
-        Debug.Log("Secondary2DAxis:.....:" + _controllerRef.ControllerInput.Secondary2DAxis);
-        Debug.Log("SecondaryTouch:......:" + _controllerRef.ControllerInput.SecondaryTouch);
+        Debug.Log("GripButton:..........:" + _rightControllerRef.ControllerInput.GripButton);
+        Debug.Log("TriggerButton:.......:" + _rightControllerRef.ControllerInput.TriggerButton);
+        Debug.Log("MenuButton:..........:" + _rightControllerRef.ControllerInput.MenuButton);
+        Debug.Log("Thumbrest:...........:" + _rightControllerRef.ControllerInput.Thumbrest);
+        Debug.Log("Primary2DAxis:.......:" + _rightControllerRef.ControllerInput.Primary2DAxis);
+        Debug.Log("Primary2DAxisClick:..:" + _rightControllerRef.ControllerInput.Primary2DAxisClick);
+        Debug.Log("Primary2DAxisTouch:..:" + _rightControllerRef.ControllerInput.Primary2DAxisTouch);
+        Debug.Log("PrimaryButton:.......:" + _rightControllerRef.ControllerInput.PrimaryButton);
+        Debug.Log("PrimaryTouch:........:" + _rightControllerRef.ControllerInput.PrimaryTouch);
+        Debug.Log("SecondaryButton:.....:" + _rightControllerRef.ControllerInput.SecondaryButton);
+        Debug.Log("Secondary2DAxis:.....:" + _rightControllerRef.ControllerInput.Secondary2DAxis);
+        Debug.Log("SecondaryTouch:......:" + _rightControllerRef.ControllerInput.SecondaryTouch);
     }
 
 }

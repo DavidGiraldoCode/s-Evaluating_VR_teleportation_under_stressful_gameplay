cedb5129391d0223f4f0ed004a468378e2ceaef0
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-10-26 14:16:25 +0200

    The anti-cheating sphere takes back the ring to the initial position

--- Assets/Scripts/Controllers/CheatingController.cs
+++ Assets/Scripts/Controllers/CheatingController.cs
@@ -1,3 +1,5 @@
+using Oculus.Interaction;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CheatingController : MonoBehaviour
@@ -8,10 +10,28 @@ public class CheatingController : MonoBehaviour
     [SerializeField] private SphereCollider _sphereCollider;
     [Tooltip("References an insivible bounding box where the ring should move, if the player takes the ring out, the component will bring the ring back to the default position")]
     [SerializeField] private BoxCollider _boxCollider;
+    [SerializeField] private Transform _defaultLocation;
+    [SerializeField] private InteractorActiveState _grabInteractorState;
+    [SerializeField] private GameObject _ISDK_HandGrabInteraction;
 
+    private void Update()
+    {
+        Debug.Log("XXX _grabInteractorState " + _grabInteractorState.Active);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("XXX OnTriggerEnter: " + other);
+        //Debug.Log("XXX OnTriggerEnter: " + other);
+        /**
+         If the player tries to pass the ring directly to the other side and hits the anticheating sphere, the ring will be set back to the default position
+        */
+        if (other == _sphereCollider)
+        {
+            Debug.Log("XXX OnTriggerEnter: " + other);
+            _ISDK_HandGrabInteraction.SetActive(false); // Disable the grabbing by force
+            gameObject.transform.position = _defaultLocation.position;
+            gameObject.transform.rotation = _defaultLocation.rotation;
+            _ISDK_HandGrabInteraction.SetActive(true);
+        }
     }
 }
 

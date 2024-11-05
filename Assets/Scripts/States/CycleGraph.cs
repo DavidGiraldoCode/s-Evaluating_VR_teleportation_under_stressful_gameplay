23d50f9044eef1d993ec7b6fcdef48156efc3482
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-11-05 11:01:36 +0100

    Updated notes

--- Assets/Scripts/States/CycleGraph.cs
+++ Assets/Scripts/States/CycleGraph.cs
@@ -5,30 +5,36 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CycleGraph", menuName = "States/CycleGraph", order = 0)]
 public class CycleGraph : ScriptableObject
 {
-    private List<int[]> nodes;
+    private List<int[]> _nodes;
+    public int NodeCount { get => _nodes.Count; }
 
     /// <summary>
-    /// Creates the graph by adding an adjacency list at each index of a `List<int[]>`, `0` for left and `1` for right clockwize.
+    /// Creates the graph by adding an adjacency list at each index of a `List<int[]>`, `0` for left and `1` for right clockwise.
+    /// It can only create graph with at least two nodes
     /// </summary>
     /// <param name="nodesCount"></param>
     public void BuildGraph(uint nodesCount)
     {
-        nodes = new List<int[]>();
+        // Invariance
+        if (nodesCount < 2) nodesCount = 2; // This makes sure the graph has at least two nodes.
+
+        _nodes = new List<int[]>();
         for (int i = 0; i < nodesCount; i++)
         {
-            int[] adjacentyList = new int[2];
+            int[] adjacencyList = new int[2];
 
-            adjacentyList[0] = (i + ((int)nodesCount - 1)) % (int)nodesCount;
-            adjacentyList[1] = (i + 1) % (int)nodesCount;
+            adjacencyList[0] = (i + ((int)nodesCount - 1)) % (int)nodesCount;
+            adjacencyList[1] = (i + 1) % (int)nodesCount;
 
-            nodes.Add(adjacentyList);
-            Debug.Log(nodes[i][0] + " <- left | node: " + i + " | right -> " + nodes[i][1]);
+            _nodes.Add(adjacencyList);
+            Debug.Log(_nodes[i][0] + " <- left | node: " + i + " | right -> " + _nodes[i][1]);
         }
     }
 
     /// <summary>
     /// The graphs retunrs the color of a node given a curren node (where the player is located) and coordinates.
-    /// The coordnates have encoded the turn (left or right) with the sign and the steps to traverse the graph
+    /// The coordnates have encoded the turn (left or right) with the sign and the steps to traverse the graph.
+    /// It will retunr -1 if invalid coordinates or an inexistant node is inputed 
     /// </summary>
     /// <param name="currentNode">Valid coordinates {-3, -2, -1, 1, 2, 3}</param>
     /// <param name="coordinates"></param>
@@ -37,22 +43,25 @@ public class CycleGraph : ScriptableObject
     {
         // Invariances
         bool invalidCoordinates = coordinates == 0 || coordinates > 3 || coordinates < -3; // The coordinate can only be {-3, -2, -1, 1, 2, 3}
-        bool invalidNode = currentNode < 0 || currentNode >= nodes.Count; // The user of this method inputed a location that does not exist.
+        bool invalidNode = currentNode < 0 || currentNode >= _nodes.Count; // The user of this method inputed a location that does not exist.
         if (invalidCoordinates || invalidNode) return -1;
 
-        bool clockWize = coordinates > 0;
+        bool clockWise = coordinates > 0;
         int steps = Math.Abs(coordinates);
         int j = 0;
         int nextNode;
-        Debug.Log("starts at " + currentNode);
+        
         while (j < steps)
         {
-            if (!clockWize) // If the turn is counter clockwize, move to the left (0) node
-                nextNode = nodes[currentNode][0];
-            else // If the turn is clockwize, move to the right (1) node
-                nextNode = nodes[currentNode][1];
+            if (!clockWise) // If the turn is counter clockwise, move to the left (0) node
+                nextNode = _nodes[currentNode][0];
+            else // If the turn is clockwise, move to the right (1) node
+                nextNode = _nodes[currentNode][1];
 
-            Debug.Log("moves to " + nextNode);
+            #if UNITY_EDITOR
+            Debug.Log($"moves to {nextNode}");
+            #endif
+
             currentNode = nextNode;
             j++;
         }
--- sessions_notes.md
+++ sessions_notes.md
@@ -1,20 +1,24 @@
 # Session notes
 
-## Session 2024-11-05: Adding graph and distances
+Here is the revised version of the README with typos and clarity improved:
 
-- **Objective:** Implement the algorithms to prompt what platform to teleport to.
-- Create the cycle graph data structure
-- Create the distances list `{-3, -2, -1, 1, 2, 3}` and shuffle it and store them in a stack
-- Use the graph to query the color that will be promted to the user
-- Pop out the distances from the color stack.
+---
 
-The graph objective is to represent colors that we can query using abstract distances, or steps, once the player is prompt to get to a new location. It is not related or dependent to the real distance in world space. Although, each vertex/node of the graph has a number that is homologous to the color of a platform. The only strong relationship between the graph and the platform in world space is the color (number) of the current platfrom the player is stading, and thus we can know from which node to start moving.
+## Session 2024-11-05: Adding Graph and Distances
 
-Considering the hexagon layout of the 6 platform (each at every vertex). The order of the platfrom needs to match the ones of the Graph nodes. So the adjacenty list of the graph relects how the platforms are distributed relative to eachother.
+- **Objective:** Implement the algorithms to determine which platform to teleport to.
+- Create the cycle graph data structure.
+- Create the list of distances `{ -3, -2, -1, 1, 2, 3 }`, shuffle it, and store the values in a stack.
+- Use the graph to query the color that will be prompted to the user.
+- Pop distances from the stack to determine the color.
+
+The graph’s purpose is to represent colors that we can query using abstract distances or steps when the player is prompted to move to a new location. This is independent of real-world distances. Each vertex/node in the graph corresponds to a platform color. The only relationship between the graph and the physical platform layout is the color (number) of the current platform the player is standing on, which allows us to determine the starting node for movement.
+
+Given the hexagonal layout of the six platforms (one at each vertex), the order of platforms should match the graph nodes. Thus, the adjacency list in the graph reflects how the platforms are distributed relative to each other.
+
 ```bash
-Platform -> red(0), blue(1), gree(2), ...
-Graph    -> {0, 1, 2, ...}
+Platform -> red(0), blue(1), green(2), ...
+Graph    -> { 0, 1, 2, ... }
 ```
 
-As the graph does not live in the scene as a GameObject, does not NEED a visual representation, or been affected by transformations. It can be a Scriptable Object.
-
+Since the graph does not exist as a GameObject in the scene, it does not need a visual representation or to be affected by transformations. Therefore, it can be implemented as a `ScriptableObject`. 
